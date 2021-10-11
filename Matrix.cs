2035f7209f15280727f750866c47d16327739a25
ak-parser <81529142+ak-parser@users.noreply.github.com>
2021-10-11 21:21:10 +0300

    Create program

---
+++ Matrix.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrix
+{
+    class Matrix: IEnumerable
+    {
+        int n;
+        int N
+        {
+            get
+            {
+                return n;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("N cannot be <= 0");
+                n = value;
+            }
+        }
+
+        int[,] array;
+        int[,] Array
+        {
+            get
+            {
+                return array;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Array not set");
+                array = value;
+            }
+        }
+
+        public int this[int i, int j]
+        {
+            get
+            {
+                if ((i < 0 || i >= N) && (j < 0 || j >= N))
+                    throw new IndexOutOfRangeException("Some of indexes are incorrect");
+                return array[i, j];
+            }
+            set
+            {
+                if ((i < 0 || i >= N) && (j < 0 || j >= N))
+                    throw new IndexOutOfRangeException("Some of indexes are incorrect");
+                array[i, j] = value;
+            }
+        }
+
+        public Matrix(int[,] array, int n)
+        {
+            N = n;
+            this.array = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    this.array[i, j] = array[i, j];
+        }
+
+        public override string ToString()
+        {
+            string info = $"Matrix info: Size: {N}\n";
+
+            foreach (var elem in this)
+                info += $"{elem} ";
+
+            return info + "\n";
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new MatrixEnumerator(Array, N);
+        }
+
+        private class MatrixEnumerator : IEnumerator
+        {
+            int positionN = -1;
+            int positionM = -1;
+            int n;
+            int[,] array;
+
+            public MatrixEnumerator(int[,] array, int n)
+            {
+                this.array = array;
+                this.n = n;
+                positionN = n - 1;
+                positionM = n;
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    try
+                    {
+                        return array[positionN, positionM];
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidOperationException("One of the index is out of MatrixEnumerator range");
+                    }
+                }
+            }
+
+            public bool MoveNext()
+            {
+                positionM--;
+                if (positionM < 0)
+                {
+                    positionM = n - 1;
+                    positionN--;
+                }
+                return positionN >= 0;
+            }
+
+            public void Reset()
+            {
+                positionN = positionM = n - 1;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[,] array = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+                Matrix matrix = new Matrix(array, 3);
+
+                //Foreach in overrided ToString() method
+                Console.WriteLine(matrix);
+            }
+            catch(ArgumentNullException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (InvalidOperationException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
